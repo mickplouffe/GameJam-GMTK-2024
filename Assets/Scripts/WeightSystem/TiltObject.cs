2efd6a7556b0331236a3b48b3f924cd63759177e
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2024-08-17 14:23:44 -0400

    formatting

--- Assets/Scripts/WeightSystem/TiltObject.cs
+++ Assets/Scripts/WeightSystem/TiltObject.cs
@@ -7,12 +7,21 @@ public class TiltObject : MonoBehaviour
 {
     [Tooltip("The severity of the angle the platform will rotate to")]
     [SerializeField] float TiltMultiplier = 10f;
+
     [Tooltip("The speed at which the platform will rotate to the desired position")]
     [SerializeField] float tiltrate;
+
     [SerializeField] GameObject centerpointmarker;
+
     [HideInInspector] public List<WeightedObject> objects = new List<WeightedObject>();
+
     Quaternion target;
+
+    [Tooltip("The angle at which the objects on the bnoard will start to slide, ~340 is a good start")]
     [SerializeField] float Slideangle;
+
+    public List<GameObject> Tiles = new List<GameObject>();
+    public List<GameObject> Enemies = new List<GameObject>();
     void Update()
     { 
        
@@ -24,6 +33,7 @@ public class TiltObject : MonoBehaviour
         Vector3 center = Vector3.zero;
         float totalweight = 0f;
         List<WeightedObject> objects = new List<WeightedObject>();
+
         foreach(WeightedObject wo in FindObjectsByType<WeightedObject>(FindObjectsSortMode.None) )
         {
             if (wo.falling == false)
@@ -35,13 +45,19 @@ public class TiltObject : MonoBehaviour
         {
             foreach (WeightedObject wo in objects)
             {
+                center.x += wo.weight * (wo.transform.position.x - transform.position.x);
+                //center.y += wo.weight * (wo.transform.position.y - transform.position.y);
+                center.z += wo.weight * (wo.transform.position.z - transform.position.z);
+                totalweight += wo.weight;
+            }
+            foreach (GameObject g in Tiles)
+            {
+                //add weight for gaem tiles
+            }
 
-               
-                    center.x += wo.weight * (wo.transform.position.x - transform.position.x);
-                    //center.y += wo.weight * (wo.transform.position.y - transform.position.y);
-                    center.z += wo.weight * (wo.transform.position.z - transform.position.z);
-                    totalweight += wo.weight;
-                
+            foreach (GameObject g in Enemies)
+            {
+                //add weight for enemies
             }
             center = center / totalweight;
         }
@@ -59,7 +75,7 @@ public class TiltObject : MonoBehaviour
         target = Quaternion.Euler(tiltAroundX * TiltMultiplier, 0, -tiltAroundZ * TiltMultiplier);
         
         Debug.Log(transform.eulerAngles.x);
-        if (Mathf.Abs(transform.eulerAngles.x) <= Slideangle || Mathf.Abs(transform.eulerAngles.z) >= Slideangle)
+        if (Mathf.Abs(transform.eulerAngles.x) <= Slideangle || Mathf.Abs(transform.eulerAngles.z) <= Slideangle)
         {
             //Debug.Log("hi");
             foreach(WeightedObject wo in objects)
