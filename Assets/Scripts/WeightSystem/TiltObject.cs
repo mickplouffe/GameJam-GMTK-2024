54680b06fe142b0c8490b9e0525952211a124eb1
evandyer1234 <62557608+evandyer1234@users.noreply.github.com>
2024-08-17 13:41:43 -0400

    objects can slip

--- Assets/Scripts/WeightSystem/TiltObject.cs
+++ Assets/Scripts/WeightSystem/TiltObject.cs
@@ -5,31 +5,77 @@ using UnityEngine;
 
 public class TiltObject : MonoBehaviour
 {
+    [Tooltip("The severity of the angle the platform will rotate to")]
     [SerializeField] float TiltMultiplier = 10f;
-    [SerializeField] GameObject centerpointmarker;
+    [Tooltip("The speed at which the platform will rotate to the desired position")]
     [SerializeField] float tiltrate;
-   
+    [SerializeField] GameObject centerpointmarker;
+    [HideInInspector] public List<WeightedObject> objects = new List<WeightedObject>();
+    Quaternion target;
+    [SerializeField] float Slideangle;
     void Update()
     { 
+       
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * tiltrate);
+    }
+
+    private void FixedUpdate()
+    {
         Vector3 center = Vector3.zero;
         float totalweight = 0f;
-
-        foreach (WeightedObject wo in FindObjectsByType<WeightedObject>(FindObjectsSortMode.None))
+        List<WeightedObject> objects = new List<WeightedObject>();
+        foreach(WeightedObject wo in FindObjectsByType<WeightedObject>(FindObjectsSortMode.None) )
         {
-
-            center.x += wo.weight * (wo.transform.position.x - transform.position.x);
-            center.y += wo.weight * (wo.transform.position.y - transform.position.y);
-            center.z += wo.weight * (wo.transform.position.z - transform.position.z);
-            totalweight += wo.weight;
+            if (wo.falling == false)
+            {
+                objects.Add(wo);
+            }
         }
-        center = center / totalweight;
+        if (objects.Count > 0)
+        {
+            foreach (WeightedObject wo in objects)
+            {
+
+               
+                    center.x += wo.weight * (wo.transform.position.x - transform.position.x);
+                    //center.y += wo.weight * (wo.transform.position.y - transform.position.y);
+                    center.z += wo.weight * (wo.transform.position.z - transform.position.z);
+                    totalweight += wo.weight;
+                
+            }
+            center = center / totalweight;
+        }
+        else
+        {
+            center = transform.position;
+        }
+       
 
         centerpointmarker.transform.position = center;
 
         float tiltAroundX = -transform.position.z + center.z;
         float tiltAroundZ = -transform.position.x + center.x;
 
-        Quaternion target = Quaternion.Euler(tiltAroundX * TiltMultiplier, 0, -tiltAroundZ * TiltMultiplier);
-        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * tiltrate);
+        target = Quaternion.Euler(tiltAroundX * TiltMultiplier, 0, -tiltAroundZ * TiltMultiplier);
+        
+        Debug.Log(transform.eulerAngles.x);
+        if (Mathf.Abs(transform.eulerAngles.x) <= Slideangle || Mathf.Abs(transform.eulerAngles.z) >= Slideangle)
+        {
+            //Debug.Log("hi");
+            foreach(WeightedObject wo in objects)
+            {
+                wo.rb.constraints = RigidbodyConstraints.None;
+                wo.transform.SetParent(null);
+            }
+        }
+        else
+        {
+            //Debug.Log("ho");
+            foreach (WeightedObject wo in objects)
+            {
+                wo.rb.constraints = RigidbodyConstraints.FreezeAll;
+                wo.transform.SetParent(this.transform);
+            }
+        }
     }
 }
